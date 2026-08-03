eb7bed8965911e2a7139db60c4e9533b9702e58a
Anna Pilyugina <anna1_89@mail.ru>
2026-08-03 20:55:49 +0300

    Added failed test

--- src/test/java/tests/RegistrationFormTests.java
+++ src/test/java/tests/RegistrationFormTests.java
@@ -2,7 +2,6 @@ package tests;
 
 import data.TestData;
 import io.qameta.allure.Story;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
