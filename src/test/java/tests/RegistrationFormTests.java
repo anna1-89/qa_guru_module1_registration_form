563aa60f117692a2a08a0b3ce554762342f1c885
Anna Pilyugina <anna1_89@mail.ru>
2026-08-03 20:26:58 +0300

    Added steps to tests

--- src/test/java/tests/RegistrationFormTests.java
+++ src/test/java/tests/RegistrationFormTests.java
@@ -1,9 +1,14 @@
 package tests;
 
 import data.TestData;
+import io.qameta.allure.Story;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
+
+@Story("Registration form")
 public class RegistrationFormTests extends TestBase {
 
     TestData testData = new TestData();
@@ -11,9 +16,12 @@ public class RegistrationFormTests extends TestBase {
     @Test
     @DisplayName("Успешная регистрация при заполнении всех полей")
     void positiveFillAllFormTest() {
-        registrationFormPage
-                .openPage()
-                .closeBanner()
+        step("Открыть форму регистрации", () ->
+                registrationFormPage.openPage());
+        step("Закрыть баннер", () ->
+                registrationFormPage.closeBanner());
+        step("Заполнить форму регистрации", () -> {
+            registrationFormPage
                 .enterFirstName(testData.firstName)
                 .enterLastName(testData.lastName)
                 .enterUserEmail(testData.email)
@@ -24,127 +32,171 @@ public class RegistrationFormTests extends TestBase {
                 .selectHobby(testData.hobby)
                 .uploadPicture(testData.fileName)
                 .enterCurrentAddress(testData.address)
-                .selectStateAndCity(testData.state, testData.city)
-                .submitForm()
-
-                .assertSuccessSubmission()
-                .checkSubmittedData("Student Name", testData.firstName + " " + testData.lastName)
-                .checkSubmittedData("Student Email", testData.email)
-                .checkSubmittedData("Gender", testData.gender)
-                .checkSubmittedData("Mobile", testData.number)
-                .checkSubmittedData("Date of Birth", testData.birthDate + " " +  testData.monthBirthDate.substring(0,3) + " " + testData.yearBirthDate)
-                .checkSubmittedData("Subjects", testData.subject)
-                .checkSubmittedData("Hobbies", testData.hobby)
-                .checkSubmittedData("Picture", testData.fileName)
-                .checkSubmittedData("Address", testData.address)
-                .checkSubmittedData("State and City", testData.state + " " + testData.city);
-    }
+                .selectStateAndCity(testData.state, testData.city);
+        });
+        step("Нажать Submit", () ->
+                registrationFormPage.submitForm());
+        step("Проверить успешность регистрации", () -> {
+            step("Проверить, что регистрация прошла успешно", () ->
+                registrationFormPage.assertSuccessSubmission());
+            step("Проверить корректность зарегистрированных данных", () -> {
+                registrationFormPage
+                        .checkSubmittedData("Student Name", testData.firstName + " " + testData.lastName)
+                        .checkSubmittedData("Student Email", testData.email)
+                        .checkSubmittedData("Gender", testData.gender)
+                        .checkSubmittedData("Mobile", testData.number)
+                        .checkSubmittedData("Date of Birth", testData.birthDate + " " + testData.monthBirthDate.substring(0, 3) + " " + testData.yearBirthDate)
+                        .checkSubmittedData("Subjects", testData.subject)
+                        .checkSubmittedData("Hobbies", testData.hobby)
+                        .checkSubmittedData("Picture", testData.fileName)
+                        .checkSubmittedData("Address", testData.address)
+                        .checkSubmittedData("State and City", testData.state + " " + testData.city);
+            });
+        });
+    };
 
     @Test
     @DisplayName("Успешная регистрация при заполнении только обязательных полей")
     void positiveFillRequiredFormTest() {
-        registrationFormPage
-                .openPage()
-                .closeBanner()
-                .enterFirstName(testData.firstName)
-                .enterLastName(testData.lastName)
-                .chooseGender(testData.gender)
-                .enterUserNumber(testData.number)
-                .submitForm()
-
-                .assertSuccessSubmission()
-                .checkSubmittedData("Student Name", testData.firstName + " " + testData.lastName)
-                .checkSubmittedData("Student Email", "-")
-                .checkSubmittedData("Gender", testData.gender)
-                .checkSubmittedData("Mobile", testData.number)
-                .checkSubmittedData("Date of Birth", "-")
-                .checkSubmittedData("Subjects", "-")
-                .checkSubmittedData("Hobbies", "-")
-                .checkSubmittedData("Picture", "-")
-                .checkSubmittedData("Address", "-")
-                .checkSubmittedData("State and City", "-");
+        step("Открыть форму регистрации", () ->
+                registrationFormPage.openPage());
+        step("Закрыть баннер", () ->
+                registrationFormPage.closeBanner());
+        step("Заполнить форму регистрации", () -> {
+            registrationFormPage
+                    .enterFirstName(testData.firstName)
+                    .enterLastName(testData.lastName)
+                    .chooseGender(testData.gender)
+                    .enterUserNumber(testData.number);
+        });
+        step("Нажать Submit", () ->
+                registrationFormPage.submitForm());
+        step("Проверить успешность регистрации", () -> {
+            step("Проверить, что регистрация прошла успешно", () ->
+                    registrationFormPage.assertSuccessSubmission());
+            step("Проверить корректность зарегистрированных данных", () -> {
+                registrationFormPage
+                        .checkSubmittedData("Student Name", testData.firstName + " " + testData.lastName)
+                        .checkSubmittedData("Student Email", "-")
+                        .checkSubmittedData("Gender", testData.gender)
+                        .checkSubmittedData("Mobile", testData.number)
+                        .checkSubmittedData("Date of Birth", "-")
+                        .checkSubmittedData("Subjects", "-")
+                        .checkSubmittedData("Hobbies", "-")
+                        .checkSubmittedData("Picture", "-")
+                        .checkSubmittedData("Address", "-")
+                        .checkSubmittedData("State and City", "-");
+            });
+        });
     }
 
     @Test
-    @DisplayName("Неуспешная регистрация при пустой форме")
+    @DisplayName("Не успешная регистрация при пустой форме")
     void negativeEmptyFormTest() {
-        registrationFormPage
-                .openPage()
-                .closeBanner()
-                .submitForm()
-
-                .assertFailSubmission();
+        step("Открыть форму регистрации", () ->
+                registrationFormPage.openPage());
+        step("Закрыть баннер", () ->
+                registrationFormPage.closeBanner());
+        step("Нажать Submit", () ->
+                registrationFormPage.submitForm());
+        step("Проверить не успешность регистрации", () ->
+                registrationFormPage.assertFailSubmission());
     }
 
     @Test
-    @DisplayName("Неуспешная регистрация при незаполненном имени")
+    @DisplayName("Не успешная регистрация при незаполненном имени")
     void negativeNotAllRequiredFistNameTest() {
-        registrationFormPage
-                .openPage()
-                .closeBanner()
-                .enterLastName(testData.lastName)
-                .chooseGender(testData.gender)
-                .enterUserNumber(testData.number)
-                .submitForm()
-
-                .assertFailSubmission();
+        step("Открыть форму регистрации", () ->
+                registrationFormPage.openPage());
+        step("Закрыть баннер", () ->
+                registrationFormPage.closeBanner());
+        step("Заполнить форму регистрации", () -> {
+            registrationFormPage
+                    .enterLastName(testData.lastName)
+                    .chooseGender(testData.gender)
+                    .enterUserNumber(testData.number);
+        });
+        step("Нажать Submit", () ->
+                registrationFormPage.submitForm());
+        step("Проверить не успешность регистрации", () ->
+                registrationFormPage.assertFailSubmission());
     }
 
     @Test
-    @DisplayName("Неуспешная регистрация при незаполненной фамилии")
+    @DisplayName("Не успешная регистрация при незаполненной фамилии")
     void negativeNotAllRequiredLastNameTest() {
-        registrationFormPage
-                .openPage()
-                .closeBanner()
-                .enterLastName(testData.firstName)
-                .chooseGender(testData.gender)
-                .enterUserNumber(testData.number)
-                .submitForm()
-
-                .assertFailSubmission();
+        step("Открыть форму регистрации", () ->
+                registrationFormPage.openPage());
+        step("Закрыть баннер", () ->
+                registrationFormPage.closeBanner());
+        step("Заполнить форму регистрации", () -> {
+            registrationFormPage
+                    .enterLastName(testData.firstName)
+                    .chooseGender(testData.gender)
+                    .enterUserNumber(testData.number);
+        });
+        step("Нажать Submit", () ->
+                registrationFormPage.submitForm());
+        step("Проверить не успешность регистрации", () ->
+                registrationFormPage.assertFailSubmission());
     }
 
     @Test
-    @DisplayName("Неуспешная регистрация при невыбранном поле")
+    @DisplayName("Не успешная регистрация при невыбранном поле")
     void negativeNotAllRequiredGenderTest() {
-        registrationFormPage
-                .openPage()
-                .closeBanner()
-                .enterLastName(testData.firstName)
-                .enterLastName(testData.lastName)
-                .enterUserNumber(testData.number)
-                .submitForm()
-
-                .assertFailSubmission();
+        step("Открыть форму регистрации", () ->
+                registrationFormPage.openPage());
+        step("Закрыть баннер", () ->
+                registrationFormPage.closeBanner());
+        step("Заполнить форму регистрации", () -> {
+            registrationFormPage
+                    .enterLastName(testData.firstName)
+                    .enterLastName(testData.lastName)
+                    .enterUserNumber(testData.number);
+        });
+        step("Нажать Submit", () ->
+                registrationFormPage.submitForm());
+        step("Проверить не успешность регистрации", () ->
+                registrationFormPage.assertFailSubmission());
     }
 
     @Test
-    @DisplayName("Неуспешная регистрация при незаполненном номера телефона")
+    @DisplayName("Не успешная регистрация при незаполненном номера телефона")
     void negativeNotAllRequiredUserNumberTest() {
-        registrationFormPage
-                .openPage()
-                .closeBanner()
-                .enterFirstName(testData.firstName)
-                .enterLastName(testData.lastName)
-                .chooseGender(testData.gender)
-                .submitForm()
-
-                .assertFailSubmission();
+        step("Открыть форму регистрации", () ->
+                registrationFormPage.openPage());
+        step("Закрыть баннер", () ->
+                registrationFormPage.closeBanner());
+        step("Заполнить форму регистрации", () -> {
+            registrationFormPage
+                    .enterLastName(testData.firstName)
+                    .enterLastName(testData.lastName)
+                    .enterUserNumber(testData.gender);
+        });
+        step("Нажать Submit", () ->
+                registrationFormPage.submitForm());
+        step("Проверить не успешность регистрации", () ->
+                registrationFormPage.assertFailSubmission());
     }
 
     @Test
-    @DisplayName("Неуспешная регистрация при некорректном номере телефона")
+    @DisplayName("Не успешная регистрация при некорректном номере телефона")
     void negativeInvalidUserNumberTest() {
-        registrationFormPage
-                .openPage()
-                .closeBanner()
-                .enterFirstName(testData.firstName)
-                .enterLastName(testData.lastName)
-                .chooseGender(testData.gender)
-                .enterUserNumber(testData.invalidNumber)
-                .submitForm()
-
-                .assertFailSubmission();
+        step("Открыть форму регистрации", () ->
+                registrationFormPage.openPage());
+        step("Закрыть баннер", () ->
+                registrationFormPage.closeBanner());
+        step("Заполнить форму регистрации", () -> {
+            registrationFormPage
+                    .enterLastName(testData.firstName)
+                    .enterLastName(testData.lastName)
+                    .enterUserNumber(testData.gender)
+                    .enterUserNumber(testData.invalidNumber);
+        });
+        step("Нажать Submit", () ->
+                registrationFormPage.submitForm());
+        step("Проверить не успешность регистрации", () ->
+                registrationFormPage.assertFailSubmission());
     }
 
 }
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,8 +1,11 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import pages.RegistrationFormPage;
 import pages.TextBoxPage;
 
@@ -12,12 +15,17 @@ public class TestBase {
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
     TextBoxPage textBoxPage = new TextBoxPage();
 
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://qa-guru.github.io/one-page-form";
 
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         Configuration.timeout = 10000;
         Configuration.pageLoadTimeout = 60000;
